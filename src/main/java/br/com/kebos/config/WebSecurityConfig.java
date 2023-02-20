97b6a7909d252bd77c3148a40a33cce26b25bc2e
cleberGraciano <cleber_batista2010@hotmail.com>
2023-02-20 10:50:56 -0300

    Criando dockerfile

--- src/main/java/br/com/kebos/config/WebSecurityConfig.java
+++ src/main/java/br/com/kebos/config/WebSecurityConfig.java
@@ -143,7 +143,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/v3/api-docs/**",
+		web.ignoring().mvcMatchers("/v3/api-docs/**",
 				"/configuration/ui",
 				"/swagger-resources/**",
 				"/configuration/security",
