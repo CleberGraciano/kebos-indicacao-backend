50f8782a6fad29690448e00704c1ddac4961fe3b
cleberGraciano <cleber_batista2010@hotmail.com>
2023-10-03 15:29:48 -0300

    Ajuste Status

--- src/main/java/br/com/kebos/dto/UserInfo.java
+++ src/main/java/br/com/kebos/dto/UserInfo.java
@@ -2,6 +2,7 @@ package br.com.kebos.dto;
 
 import java.util.List;
 
+import br.com.kebos.model.User;
 import lombok.Value;
 
 @Value
@@ -9,4 +10,6 @@ public class UserInfo {
 	private String id, displayName, email;
 	private boolean statusCadastro;
 	private List<String> roles;
+
 }
+
